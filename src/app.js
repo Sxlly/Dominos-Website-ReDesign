7f6c1f13223d0c93410c9390c7f3e5667173a58a
Sxlly <shaesullivan.buisness1@gmail.com>
2021-05-04 12:26:44 +0800

    menu functionality

--- src/app.js
+++ src/app.js
@@ -29,6 +29,45 @@ function closeNav() {
 
 /* menu js */
 
+if (document.readyState == 'loading') {
+
+    document.addEventListener('DOMcontentLoaded', ready)
+}
+
+else {
+
+    ready()
+}
+
+function ready() {
+
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+
+    for (var index = 0; index < removeCartItemButtons.length; index++) {
+
+        var button = removeCartItemButtons[index]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+
+    for (var index = 0; index < quantityInputs.length; index++) {
+
+        var input = quantityInputs[index]
+        input.addEventListener('change', quantityChanged)
+    }
+
+    var addToCartButton = document.getElementsByClassName('shop-item-button')
+
+    for (var index = 0; index < addToCartButton.length; index++) {
+
+        var button = addToCartButton[index]
+        button.addEventListener('click', addToCartButton)
+    }
+}
+
+
+
 
 
 
