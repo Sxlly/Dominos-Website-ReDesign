3dd5d8613552e2a1e482ada70598bf65ff0ec708
Sxlly <shaesullivan.buisness1@gmail.com>
2021-05-12 18:11:19 +0800

    video added

--- src/app.js
+++ src/app.js
@@ -249,7 +249,44 @@ function initAutocomplete() {
       });
       map.fitBounds(bounds);
     });
-  }
+}
+
+var video = document.querySelector('.video');
+var juice = document.querySelector('.orange-juice');
+var btn = document.getElementById('play-pause');
+
+function togglePlayPause() {
+
+    if (video.paused) {
+
+        btn.className = 'pause';
+        video.play();
+    }
+
+    else {
+
+        btn.className = 'play';
+        video.pause();
+    }
+}
+
+btn.onclick = function (params) {
+
+    togglePlayPause();
+}
+
+video.addEventListener('timeupdate', function() {
+
+    var juicePos = video.currentTime / video.duration;
+    juice.style.width = juicePos * 100 + "%";
+
+    if (video.ended) {
+
+        btn.className = "play";
+        video.currentTime = 0;
+        togglePlayPause();
+    }
+});
 
 
 
